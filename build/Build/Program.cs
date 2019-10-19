ca7692f36552109a43110c41aa2d2a64cfce8878
Paul Knopf <pauldotknopf@gmail.com>
2019-10-19 13:00:44 -0400

    First commit.

---
+++ build/Build/Program.cs
@@ -0,0 +1,24 @@
+﻿using Build.Buildary;
+using Build.Common;
+using static Bullseye.Targets;
+
+namespace Build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = Runner.ParseOptions<Runner.RunnerOptions>(args);
+            
+            ProjectDefinition.Register(options, new ProjectDefinition
+            {
+                ProjectName = "Identity Server",
+                SolutionPath = "./PlumPack.IdentityServer.sln",
+                WebProjectPath = "./src/PlumPack.IdentityServer.Web/PlumPack.IdentityServer.Web.csproj",
+                DockerImageName = "plumpack/identity-server"
+            });
+            
+            Runner.Execute(options);
+        }
+    }
+}
