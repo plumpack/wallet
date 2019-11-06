36c58ec1dc93b63b24ce6712d956f1d6d889b490
Paul Knopf <pauldotknopf@gmail.com>
2019-11-06 01:50:05 -0500

    Working on using vuejs.

--- assets/plumpack-wallet/src/scripts/scripts.ts
+++ assets/plumpack-wallet/src/scripts/scripts.ts
@@ -1,5 +1,9 @@
 import Api from "plumpack-assets/src/scripts/api";
 import PayPal from "./paypal";
+import Vue from "vue";
+import VueCurrencyInput from 'vue-currency-input';
+
+Vue.use(VueCurrencyInput);
 
 declare global {
     interface Window { baseUrl: string; }
@@ -8,5 +12,25 @@ declare global {
 export let Wallet = { 
     payPal: function() {
         return new PayPal(new Api(window.baseUrl));
+    },
+    currencyInput(element: string, culture: string, currency: string, currentValue: number, changed: (n: number) => null) {
+        new Vue({
+            el: element,
+            data: {
+                value: currentValue,
+                culture: culture,
+                currency: currency
+            },
+            computed: {
+                numberValue() {
+                    return this.$parseCurrency(this.value, this.culture, this.currency)
+                }
+            },
+            watch: {
+                numberValue: function (value) {
+                    changed(Number(value));
+                }
+            }
+        })
     }
 }
--- src/PlumPack.Wallet.Web/Features/AddFunds/Models/AddFundsViewModel.cs
+++ src/PlumPack.Wallet.Web/Features/AddFunds/Models/AddFundsViewModel.cs
@@ -3,5 +3,7 @@ namespace PlumPack.Wallet.Web.Features.AddFunds.Models
     public class AddFundsViewModel
     {
         public string PayPalClientId { get; set; }
+        
+        public int Amount { get; set; }
     }
 }
